92d661331ba7344b700c3d6b6695e438cc4ec6ce
Pablotero05 <pablotero2daw@gmail.com>
2026-02-27 14:35:32 +0100

    Añade LICENSE AGPL-3.0

---
+++ scripts/mobileMenu.js
@@ -0,0 +1,23 @@
+  
+  const toggleBtn = document.querySelector('[data-mobile-menu-toggle]');
+  const mobileMenu = document.querySelector('[data-mobile-menu]');
+
+  if (toggleBtn && mobileMenu) {
+    toggleBtn.addEventListener('click', () => {
+      const isClosed = mobileMenu.classList.contains('max-h-0');
+
+      if (isClosed) {
+        mobileMenu.classList.remove('max-h-0');
+        mobileMenu.classList.add('max-h-96'); // ajusta altura según necesites
+        mobileMenu.classList.remove('pb-0');
+        mobileMenu.classList.add('pb-6');
+        toggleBtn.setAttribute('aria-expanded', 'true');
+      } else {
+        mobileMenu.classList.remove('max-h-96');
+        mobileMenu.classList.add('max-h-0');
+        mobileMenu.classList.remove('pb-6');
+        mobileMenu.classList.add('pb-0');
+        toggleBtn.setAttribute('aria-expanded', 'false');
+      }
+    });
+  }
